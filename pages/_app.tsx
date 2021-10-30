40657b07c098147ee8dc7a4b2bd2a1848fd12c92
Paul Olajide <pauleke95@gmail.com>
2021-10-30 13:45:30 +0100

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { customTheme } from "theme";
+import { AnimatePresence } from "framer-motion";
+import { useRouter } from "next/dist/client/router";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const headerName = (() => {
+    const currentPath = router.pathname?.split("/")[1];
+    if (currentPath) {
+      return `${currentPath.charAt(0).toUpperCase()}${currentPath.slice(1)}`;
+    }
+    return "Home";
+  })();
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Head>
+        <title>{headerName}</title>
+      </Head>
+      <AnimatePresence>
+        <Component {...pageProps} />
+      </AnimatePresence>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
